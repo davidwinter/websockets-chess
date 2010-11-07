676efb17d2c167f5f93fe7de86d02f00a982cb02
David Winter <i@davidwinter.me.uk>
2010-11-07 16:58:43 +0000

    Server can now accept multiple players, starting new games as needed.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 var ws = require('websocket-server');
-var players = ['White', 'Black'];
+var players = {};
+var waitingPlayers = [];
 
 function init() {
 	socket = ws.createServer();
@@ -11,11 +12,25 @@ function init() {
 	socket.addListener('connection', function(client) {
 		console.log('Connected: '+client.id);
 		
-		client.send(JSON.stringify({type: 1, data: players.shift()}));
+		if (waitingPlayers.length > 0) {
+			// Players already waiting
+			white = waitingPlayers.shift();
+			black = client.id;
+			players[white] = {opponent: black, colour: 'Black'};
+			players[black] = {opponent: white, colour: 'White'};
+			socket.send(white, JSON.stringify({type: 1, data: 'White'}));
+			socket.send(black, JSON.stringify({type: 1, data: 'Black'}));
+		}
+		else {
+			waitingPlayers.push(client.id);
+		}
+		
+		//client.send(JSON.stringify({type: 1, data: players.shift()}));
 		
 		client.addListener('message', function(message) {
 			console.log(client.id+' moves: '+message);
-			socket.broadcast(message);
+			socket.send(players[client.id]['opponent'], message);
+			//socket.broadcast(message);
 		});
 	});
 	
